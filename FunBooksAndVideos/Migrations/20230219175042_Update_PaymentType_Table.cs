5bda96b27094e024e299487d6845a57e59bbdb46
cosmincalin <Cosmin.Calin@aerlingus.com>
2023-02-19 19:51:20 +0200

    - added migration for PaymentType table

---
+++ FunBooksAndVideos/Migrations/20230219175042_Update_PaymentType_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FunBooksAndVideos.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePaymentTypeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "PaymentType",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "PaymentType");
+        }
+    }
+}
